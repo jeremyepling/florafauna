d379b932b79683a5628c52a3e21d9ddb230941d7
Jeremy Epling <jeremy.epling@gmail.com>
2025-01-01 19:37:56 -0500

    - Add Gecko sounds using other mob sounds

--- src/main/java/net/j40climb/florafauna/event/EntityEvents.java
+++ src/main/java/net/j40climb/florafauna/event/EntityEvents.java
@@ -4,10 +4,14 @@ import net.j40climb.florafauna.FloraFauna;
 import net.j40climb.florafauna.entity.ModEntities;
 import net.j40climb.florafauna.entity.client.GeckoModel;
 import net.j40climb.florafauna.entity.custom.GeckoEntity;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.event.entity.EntityAttributeCreationEvent;
+import net.neoforged.neoforge.event.entity.RegisterSpawnPlacementsEvent;
 
 @EventBusSubscriber(modid = FloraFauna.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class EntityEvents {
@@ -22,9 +26,11 @@ public class EntityEvents {
         event.put(ModEntities.GECKO.get(), GeckoEntity.createAttributes().build());
     }
 
-//    @SubscribeEvent
-//    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
-//        event.register(ModEntities.GECKO.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
-//                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
-//    }
+    // Manually create the spawn file since I don't have worldgen. Create file in src/generated/resources/data/florafauna/neoforge/biome_modifier/spawn_gecko.json
+    // https://github.com/Tutorials-By-Kaupenjoe/NeoForge-Tutorial-1.21.X/compare/40-entityVariant...41-spawnEntity
+    @SubscribeEvent
+    public static void registerSpawnPlacements(RegisterSpawnPlacementsEvent event) {
+        event.register(ModEntities.GECKO.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                Animal::checkAnimalSpawnRules, RegisterSpawnPlacementsEvent.Operation.REPLACE);
+    }
 }
